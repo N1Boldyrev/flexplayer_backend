c097ee12086bb757d948256a9ba8c1cf33075518
Болдырев Никита <boldyrev.ns@reliab.tech>
2021-08-22 12:07:44 +0300

    Добавил настройки для CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
+  app.use(helmet());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
